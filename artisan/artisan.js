183b279f127833a1854a717bf8064e7da7255f9d
nvphan <nvphan@enjoyworks.co.kr>
2025-04-14 11:18:17 +0700

    fix: artisan for windows

--- artisan/artisan.js
+++ artisan/artisan.js
@@ -1,13 +1,19 @@
-const {exec} = require('child_process');
-const {spawn} = require('child_process');
+
+const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const command = process.argv[2];
 const param = process.argv[3];
+
+const isWin = process.platform === 'win32';
+const yarnCmd = isWin ? 'yarn.cmd' : 'yarn';
+const npxCmd = isWin ? 'npx.cmd' : 'npx';
+const nodeCmd = isWin ? 'node.exe' : 'node';
 
 switch (command) {
   case 'dev':
   case 'server':
   case 'serve':
-    const child = spawn('yarn', [command], {stdio: 'inherit'});
+    const child = spawn(yarnCmd, [command], { stdio: 'inherit' });
     child.on('close', (code) => {
       process.exit(code);
     });
@@ -17,92 +23,72 @@ switch (command) {
       console.log('Module name is required.');
       process.exit(1);
     }
-    exec(`node artisan/make/make-module.js ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeModule = spawn(nodeCmd, ['artisan/make/make-module.js', param], { stdio: 'inherit' });
+    makeModule.on('close', code => process.exit(code));
     break;
   case 'make:model':
     if (!param) {
       console.log('Model name is required.');
       process.exit(1);
     }
-    exec(`npx sequelize-cli model:generate --name ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeModel = spawn(npxCmd, ['sequelize-cli', 'model:generate', '--name', param], { stdio: 'inherit' });
+    makeModel.on('close', code => process.exit(code));
     break;
   case 'make:migration':
     if (!param) {
       console.log('Migration name is required.');
       process.exit(1);
     }
-    exec(`npx sequelize-cli migration:generate --name ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeMigration = spawn(npxCmd, ['sequelize-cli', 'migration:generate', '--name', param], { stdio: 'inherit' });
+    makeMigration.on('close', code => process.exit(code));
     break;
   case 'migrate':
-    exec('npx sequelize-cli db:migrate', (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const migrate = spawn(npxCmd, ['sequelize-cli', 'db:migrate'], { stdio: 'inherit' });
+    migrate.on('close', code => process.exit(code));
     break;
   case 'migrate:undo':
-    exec('npx sequelize-cli db:migrate:undo', (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const migrateUndo = spawn(npxCmd, ['sequelize-cli', 'db:migrate:undo'], { stdio: 'inherit' });
+    migrateUndo.on('close', code => process.exit(code));
     break;
   case 'seed':
-    exec('npx sequelize-cli db:seed:all', (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const seed = spawn(npxCmd, ['sequelize-cli', 'db:seed:all'], { stdio: 'inherit' });
+    seed.on('close', code => process.exit(code));
     break;
   case 'make:seed':
     if (!param) {
       console.log('Seeder name is required.');
       process.exit(1);
     }
-    exec(`npx sequelize-cli seed:generate --name ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeSeed = spawn(npxCmd, ['sequelize-cli', 'seed:generate', '--name', param], { stdio: 'inherit' });
+    makeSeed.on('close', code => process.exit(code));
     break;
   case 'tree-structure':
-    exec('tree -d -L 3 --noreport --charset=ascii -I "node_modules|.git|dist|uploads" > structure.txt', (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const treeCmd = isWin ? 'tree.com' : 'tree';
+    const treeArgs = isWin
+      ? ['/F', '/A']
+      : ['-d', '-L', '3', '--noreport', '--charset=ascii', '-I', 'node_modules|.git|dist|uploads'];
+    const tree = spawn(treeCmd, treeArgs, { stdio: 'inherit' });
+    tree.on('close', code => process.exit(code));
     break;
   case 'queue:work':
-    const worker = spawn('node', ['worker.js', '--tries', param || '3'], {
-      stdio: 'inherit',
-    });
-    worker.on('close', (code) => {
-      process.exit(code);
-    });
+    const worker = spawn(nodeCmd, ['worker.js', '--tries', param || '3'], { stdio: 'inherit' });
+    worker.on('close', code => process.exit(code));
     break;
   case 'make:job':
     if (!param) {
       console.log('❗️ Job name is required: yarn artisan make:job SendMailJob');
       process.exit(1);
     }
-    exec(`node artisan/make/make-job.js ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeJob = spawn(nodeCmd, ['artisan/make/make-job.js', param], { stdio: 'inherit' });
+    makeJob.on('close', code => process.exit(code));
     break;
   case 'make:schedule':
     if (!param) {
       console.log('❗ Schedule name is required: yarn artisan make:schedule CleanLogJob');
       process.exit(1);
     }
-    exec(`node artisan/make/make-schedule.js ${param}`, (err, stdout, stderr) => {
-      if (err) console.error(stderr);
-      else console.log(stdout);
-    });
+    const makeSchedule = spawn(nodeCmd, ['artisan/make/make-schedule.js', param], { stdio: 'inherit' });
+    makeSchedule.on('close', code => process.exit(code));
     break;
 
   default:
